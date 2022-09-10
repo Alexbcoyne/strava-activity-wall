1d051507d18dfd543a4c12bc00a0caa176d2c9d9
Alexbcoyne <alexbcoyne@gmail.com>
2022-09-10 13:29:32 +0100

    produce dates on localhost

--- strava_api.js
+++ strava_api.js
@@ -3,17 +3,31 @@ const auth_link = "https://www.strava.com/oauth/token";
 function getActivities(res) {
     const activities_link = `https://www.strava.com/api/v3/athlete/activities?access_token=${res.access_token}`;
     var cal = new CalHeatMap();
+    var datas = [
+        {date: 1660375704, value: 15},
+        {date: 1660548504, value: 25},
+        {date: 1660725704, value: 10},
+    ];
+    var parser = function(data) {
+        var stats = {};
+        for (var d in data) {
+            stats[data[d].date] = data[d].value;
+        }
+        return stats;
+    };
+
     fetch(activities_link)
         .then(res => res.json())
         .then(json => {
             for (let i = 0; i < json.length; i++) {
-                let date = json[i].start_date
+                var date = json[i].start_date
                 console.log(date);
             }
             cal.init({
                 domain: "year",
                 subDomain: "day",
-                //data: date,
+                data: datas,
+                afterLoadData: parser,
                 cellSize: 16,
                 range: 1,
                 legend: [20, 40, 60, 80]
